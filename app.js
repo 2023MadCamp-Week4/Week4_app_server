e528cd5264837a9a8e73cc31276baa9a981abb50
frica12 <fricaa@naver.com>
2023-07-24 18:00:56 +0900

    0724 18:00

--- app.js
+++ app.js
@@ -125,3 +125,22 @@ app.get("/api/get_all_users", async (req, res) => {
     res.status(500).json({ message: err.message });
   }
 });
+
+// 상태 메시지 변경
+app.post("/api/update_status_message", async (req, res) => {
+  const { id, statusMessage } = req.body;
+
+  try {
+    const [result] = await pool.query(
+      "UPDATE users SET statusMessage = ? WHERE kakao_id = ?",
+      [statusMessage, id]
+    );
+    if (result.affectedRows > 0) {
+      res.status(200).json({ message: "Status message updated successfully!" });
+    } else {
+      res.status(404).json({ message: "User not found" });
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
