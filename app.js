de0ed78a8ef5bcf1da1f0d64e08096c85d7a54d1
frica12 <fricaa@naver.com>
2023-07-27 01:42:05 +0900

    0727 01:42

--- app.js
+++ app.js
@@ -284,3 +284,21 @@ app.post("/api/send_request", async (req, res) => {
     res.status(500).json({ message: err.message });
   }
 });
+
+// 이름으로 사용자의 kakao_id 조회
+app.get("/api/get_userid_byname", async (req, res) => {
+  const { name } = req.query;
+  try {
+    const [rows] = await pool.query(
+      "SELECT kakao_id FROM users WHERE name = ?",
+      [name]
+    );
+    if (rows.length > 0) {
+      res.json(rows[0].kakao_id);
+    } else {
+      res.status(404).json({ message: "User not found" });
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
